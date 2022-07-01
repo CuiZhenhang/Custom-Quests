353ff0291a4d43b8af0769aa24a2e3c0bb1fed44
CuiZhenhang <3243254544@qq.com>
2022-07-01 18:23:46 +0800

    Add files via upload

--- launcher.js
+++ launcher.js
@@ -1,6 +1,6 @@
 ConfigureMultiplayer({
-    name: "Custom Quests",
-    version: "2.0.0-alpha.1",
+    name: 'Custom Quests',
+    version: '2.0.0-alpha.1',
     isClientOnly: false
-});
-Launch();
+})
+Launch()
